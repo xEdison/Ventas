0d351c06b8609c475f5f5c5e586f3d34c5c40a0e
adverikg <andreselgamer1@gmail.com>
2023-12-07 16:48:23 -0500

    se agrega funciones

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@ function closeAlert() {
 setTimeout(function() {
     document.getElementById("myAlert").style.display = "block";
 }, 3);
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.getElementById('colores').querySelectorAll('button');
+    const images = document.querySelectorAll('.products .product');
+    const mostrarTodoButton = document.getElementById('mostrarTodo');
+
+    // Asocia un evento click a cada botón de colores
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const color = this.id;
+            filterImagesByColor(color);
+        });
+    });
+
+    // Asocia un evento click al botón "Mostrar Todo"
+    mostrarTodoButton.addEventListener('click', function() {
+        images.forEach(image => {
+            image.style.display = 'flex';
+        });
+    });
+
+    // Función para mostrar u ocultar imágenes según el color
+    function filterImagesByColor(color) {
+        images.forEach(image => {
+            const productColor = image.getAttribute('data-color');
+            if (color === 'todos' || productColor === color) {
+                image.style.display = 'flex';
+            } else {
+                image.style.display = 'none';
+            }
+        });
+    }
+});
